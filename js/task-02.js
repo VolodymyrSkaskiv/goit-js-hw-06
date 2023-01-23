46431718472f2a1e44bc7d550a2c08b098e92fe3
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-01-23 19:16:28 +0200

    Update task-02.js
    add one <li>

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ul = document.querySelector("#ingredients");
+console.log(ul);
+
+const li = document.createElement("li");
+li.textContent = ingredients[0];
+li.classList.add("item");
+console.log(li);
+
+ul.append(li);
