3f45dffbb61e71e0d530adfb09c433f13bdba72b
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-01-28 00:29:45 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const refs = {
+  btn: document.querySelector("button.change-color"),
+  span: document.querySelector("span.color"),
+  body: document.querySelector("body"),
+};
+
+refs.btn.addEventListener("click", onChangeBackground);
+
+function onChangeBackground() {
+  refs.span.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = refs.span.textContent;
 }
