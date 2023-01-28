92a7cf1cbf6d2aa90109020a9e25530384f1e7ec
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-01-28 18:46:56 +0200

    task10
    refs
    destroyBoxes()

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector("input"),
+  btnCreate: document.querySelector('[data-create="create"]'),
+  btnDestroy: document.querySelector('[data-destroy="destroy"]'),
+  divBoxes: document.querySelector("#boxes"),
+};
+
+console.log(refs.input);
+console.log(refs.btnCreate);
+console.log(refs.btnDestroy);
+console.log(refs.divBoxes);
+
+refs.input.addEventListener("input", onInputNumber);
+refs.btnCreate.addEventListener("click", createBoxes);
+refs.btnDestroy.addEventListener("click", destroyBoxes);
+
+function onInputNumber() {
+  console.log("input");
+}
+
+function createBoxes(amount) {
+  console.log("createBoxes");
+}
+
+function destroyBoxes() {
+  console.log("destroyBoxes");
+  console.log(refs.divBoxes.innerHTML);
+  refs.divBoxes.innerHTML = "";
+  console.log(refs.divBoxes.innerHTML);
+}
